478c51375083c5882c649e7f6758925329cf8b92
akshita2903 <akshita29320@gmail.com>
2023-01-30 17:32:55 +0530

    modules

---
+++ practical/day3/Module_exprt_imprt/exportedFile.js
@@ -0,0 +1,12 @@
+const add=(a,b)=>{
+    return a+b;
+};
+const sub=(a,b)=>{
+    return a-b;
+}
+// const name="Ak";
+// var module: {
+    // exports: typeof module.exports;
+// }
+
+module.exports={add,sub};
